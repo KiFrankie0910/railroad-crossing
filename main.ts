7bde9b661531d2f822a2a40d069ad1c7fa6d7755
Ki Frankie <106165688+kifrankie0910@users.noreply.github.com>
2023-03-17 15:50:11 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
 input.onButtonPressed(Button.A, function () {
+    Train_Passing = 1
     pins.servoWritePin(AnalogPin.P1, 90)
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
 })
 input.onButtonPressed(Button.B, function () {
+    Train_Passing = 0
     pins.servoWritePin(AnalogPin.P1, 0)
     strip.showColor(neopixel.colors(NeoPixelColors.Green))
 })
+let Train_Passing = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P2, 1, NeoPixelMode.RGB)
 pins.servoWritePin(AnalogPin.P1, 0)
 strip.showColor(neopixel.colors(NeoPixelColors.Green))
 basic.forever(function () {
-    let Train_Passing = 0
     if (Train_Passing == 1) {
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         basic.pause(100)
